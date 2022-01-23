b7541ff2725c1a4241a8fa0694dd058e5f7312bb
Can Demircan <53577964+kaypuz@users.noreply.github.com>
2022-01-23 23:46:12 +0300

    Version

--- version.lua
+++ version.lua
@@ -12,7 +12,7 @@ function GetCurrentVersion()
 end
 
 -- Grabs the latest version number from the web GitHub
-PerformHttpRequest( "https://wolfknight98.github.io/wk_wars2x_web/version.txt", function( err, text, headers )
+PerformHttpRequest( "--https://wolfknight98.github.io/wk_wars2x_web/version.txt--", function( err, text, headers )
 	-- Wait to reduce spam
 	Citizen.Wait( 2000 )
 
@@ -30,17 +30,17 @@ PerformHttpRequest( "https://wolfknight98.github.io/wk_wars2x_web/version.txt", 
 
 		-- If the versions are different, print it out
 		if ( text ~= curVer ) then
-			print( "  ||    ^1Your Wraith ARS 2X version is outdated, visit the FiveM forum post to get the latest version.\n^0  \\\\\n" )
+			print( "  ||    ^1NewCarPackKaypuz sürümünüz güncel değil, en son sürümü almak için Github gönderisini ziyaret edin.\n^0  \\\\\n" )
 		else
-			print( "  ||    ^2 NewCarPackKaypuz Güncel!\n^0  ||\n  \\\\\n" )
+			print( "  ||    ^2NewCarPackKaypuz Güncel!\n^0  ||\n  \\\\\n" )
 		end
 	else
 		-- In case the version can not be requested, print out an error message
-		print( "  ||    ^1There was an error getting the latest version information.\n^0  ||\n  \\\\\n" )
+		print( "  ||    ^1En son sürüm bilgileri alınırken bir hata oluştu.\n^0  ||\n  \\\\\n" )
 	end
 
 	-- Warn the console if the resource has been renamed, as this will cause issues with the resource's functionality.
-	if ( GetCurrentResourceName() ~= "wk_wars2x" ) then
-		print( "^1ERROR: Resource name is not wk_wars2x, expect there to be issues with the resource. To ensure there are no issues, please leave the resource name as wk_wars2x^0\n\n" )
+	if ( GetCurrentResourceName() ~= "NewCarPackKaypuz" ) then
+		print( "^1Hata: Kaynak adı NewCarPackKaypuz değil, kaynakla ilgili sorunlar olmasını bekliyoruz. Herhangi bir sorun olmadığından emin olmak için lütfen kaynak adını şu şekilde bırakın NewCarPackKaypuz^0\n\n" )
 	end
 end )
