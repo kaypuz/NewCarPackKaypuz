e50096726bd1ccbed66b38db0b0b9f257c62445c
Can Demircan <53577964+kaypuz@users.noreply.github.com>
2022-01-23 18:49:31 +0300

    Big Update

--- data/aracisim.lua
+++ data/aracisim.lua
@@ -1,4 +1,6 @@
 Citizen.CreateThread(function()
-    AddTextEntry('panamera17turbo', 'Porsche Panamera')
+    AddTextEntry('22g63', 'Mercedes Benz G63 2022')
+    AddTextEntry('22m5', 'BMW M5CS F90 2022')
+    AddTextEntry('panamera17turbo', 'Porsche Panamera 2017')
 
 end)
