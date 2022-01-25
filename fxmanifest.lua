504df4ac5bb383ae90c897267fe44950d9e20be5
Can Demircan <53577964+kaypuz@users.noreply.github.com>
2022-01-25 23:08:55 +0300

    Big Update

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,16 +5,16 @@ game 'gta5'
 name "NewCarPackKaypuz"
 description "Araç"
 author "Kaypuz"
-version "1.0.4"
+version "1.0.7"
 
 
 files {
     'data/**/*.meta',
     'data/**/*.lua',
-    'audionconfig/brabv8_game.dat151.rel',
-    'audioconfig/brabv8_sounds.dat54.rel',
-    'sfx/dlc_brabv8/brabv8.awc',
-    'sfx/dlc_brabv8/brabv8_npc.awc'
+	"audioconfig/*.dat151.rel",
+	"audioconfig/*.dat54.rel",
+	"audioconfig/*.dat10.rel",
+	"sfx/**/*.awc"
 }
 
 server_script "version.lua"
@@ -24,6 +24,15 @@ data_file 'VEHICLE_METADATA_FILE' 'data/**/vehicles.meta'
 data_file 'CARCOLS_FILE' 'data/**/carcols.meta'
 data_file 'VEHICLE_VARIATION_FILE' 'data/**/carvariations.meta'
 data_file 'VEHICLE_LAYOUTS_FILE' 'data/**/vehiclelayouts.meta'
-data_file 'AUDIO_GAMEDATA' 'audioconfig/brabv8_game.dat'
-data_file 'AUDIO_SOUNDDATA' 'audioconfig/brabv8_sounds.dat'
-data_file 'AUDIO_WAVEPACK' 'sfx/dlc_brabv8'
+data_file "AUDIO_SYNTHDATA" "audioconfig/lamavgineng_amp.dat"
+data_file "AUDIO_GAMEDATA" "audioconfig/lamavgineng_game.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/lamavgineng_sounds.dat"
+data_file "AUDIO_WAVEPACK" "sfx/dlc_lamavgineng"
+data_file "AUDIO_SYNTHDATA" "audioconfig/camls3v8_amp.dat"
+data_file "AUDIO_GAMEDATA" "audioconfig/camls3v8_game.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/camls3v8_sounds.dat"
+data_file "AUDIO_WAVEPACK" "sfx/dlc_camls3v8"
+data_file 'AUDIO_SYNTHDATA' 'audioconfig/nisgtr35_amp.dat'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/nisgtr35_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/nisgtr35_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_nisgtr35'
