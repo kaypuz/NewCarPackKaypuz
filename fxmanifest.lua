a70a87c57b095cdf36b7a413191641f0cbca8802
Can Demircan <53577964+kaypuz@users.noreply.github.com>
2022-01-25 20:10:21 +0300

    Update

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -11,6 +11,10 @@ version "1.0.3"
 files {
     'data/**/*.meta',
     'data/**/*.lua',
+    'audionconfig/brabv8_game.dat151.rel',
+    'audioconfig/brabv8_sounds.dat54.rel',
+    'sfx/dlc_brabv8/brabv8.awc',
+    'sfx/dlc_brabv8/brabv8_npc.awc'
 }
 
 server_script "version.lua"
@@ -20,3 +24,6 @@ data_file 'VEHICLE_METADATA_FILE' 'data/**/vehicles.meta'
 data_file 'CARCOLS_FILE' 'data/**/carcols.meta'
 data_file 'VEHICLE_VARIATION_FILE' 'data/**/carvariations.meta'
 data_file 'VEHICLE_LAYOUTS_FILE' 'data/**/vehiclelayouts.meta'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/brabv8_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/brabv8_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_brabv8'
