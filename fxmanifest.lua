3e35fe5436e42036fb7c30bcc7b0b413bd737b4b
Can Demircan <53577964+kaypuz@users.noreply.github.com>
2022-01-26 16:26:49 +0300

    update

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,7 +5,7 @@ game 'gta5'
 name "NewCarPackKaypuz"
 description "Araç"
 author "Kaypuz"
-version "1.0.7"
+version "1.0.8"
 
 
 files {
@@ -28,18 +28,19 @@ data_file 'AUDIO_SYNTHDATA' 'audioconfig/bnr34ffeng_amp.dat'
 data_file 'AUDIO_GAMEDATA' 'audioconfig/bnr34ffeng_game.dat'
 data_file 'AUDIO_SOUNDDATA' 'audioconfig/bnr34ffeng_sounds.dat'
 data_file 'AUDIO_WAVEPACK' 'sfx/dlc_bnr34ffeng'
-
 data_file 'AUDIO_SYNTHDATA' 'audioconfig/camls3v8_amp.dat'
 data_file 'AUDIO_GAMEDATA' 'audioconfig/camls3v8_game.dat'
 data_file 'AUDIO_SOUNDDATA' 'audioconfig/camls3v8_sounds.dat'
 data_file 'AUDIO_WAVEPACK' 'sfx/dlc_camls3v8'
-
-data_file "AUDIO_SYNTHDATA" "audioconfig/lamavgineng_amp.dat"
-data_file "AUDIO_GAMEDATA" "audioconfig/lamavgineng_game.dat"
-data_file "AUDIO_SOUNDDATA" "audioconfig/lamavgineng_sounds.dat"
-data_file "AUDIO_WAVEPACK" "sfx/dlc_lamavgineng"
-
-data_file 'AUDIO_SYNTHDATA' 'audioconfig/bnr34ffeng_amp.dat'
-data_file 'AUDIO_GAMEDATA' 'audioconfig/bnr34ffeng_game.dat'
-data_file 'AUDIO_SOUNDDATA' 'audioconfig/bnr34ffeng_sounds.dat'
-data_file 'AUDIO_WAVEPACK' 'sfx/dlc_bnr34ffeng'
+data_file 'AUDIO_SYNTHDATA' 'audioconfig/f136_amp.dat'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/f136_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/f136_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_f136'
+data_file 'AUDIO_SYNTHDATA' 'audioconfig/lamavgineng_amp.dat'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/lamavgineng_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/lamavgineng_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_lamavgineng'
+data_file 'AUDIO_SYNTHDATA' 'audioconfig/nisgtr35_amp.dat'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/nisgtr35_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/nisgtr35_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_nisgtr35'
