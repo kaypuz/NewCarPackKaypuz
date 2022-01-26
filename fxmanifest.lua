22a36ec3642d766edd31de013c44a5b1a0100b4e
Can Demircan <53577964+kaypuz@users.noreply.github.com>
2022-01-26 16:24:06 +0300

    sound

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -24,7 +24,22 @@ data_file 'VEHICLE_METADATA_FILE' 'data/**/vehicles.meta'
 data_file 'CARCOLS_FILE' 'data/**/carcols.meta'
 data_file 'VEHICLE_VARIATION_FILE' 'data/**/carvariations.meta'
 data_file 'VEHICLE_LAYOUTS_FILE' 'data/**/vehiclelayouts.meta'
+data_file 'AUDIO_SYNTHDATA' 'audioconfig/bnr34ffeng_amp.dat'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/bnr34ffeng_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/bnr34ffeng_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_bnr34ffeng'
+
+data_file 'AUDIO_SYNTHDATA' 'audioconfig/camls3v8_amp.dat'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/camls3v8_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/camls3v8_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_camls3v8'
+
 data_file "AUDIO_SYNTHDATA" "audioconfig/lamavgineng_amp.dat"
 data_file "AUDIO_GAMEDATA" "audioconfig/lamavgineng_game.dat"
 data_file "AUDIO_SOUNDDATA" "audioconfig/lamavgineng_sounds.dat"
 data_file "AUDIO_WAVEPACK" "sfx/dlc_lamavgineng"
+
+data_file 'AUDIO_SYNTHDATA' 'audioconfig/bnr34ffeng_amp.dat'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/bnr34ffeng_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/bnr34ffeng_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_bnr34ffeng'
