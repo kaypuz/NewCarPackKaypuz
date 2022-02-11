4a3d83da11b9b197f908a2f6b56dc39a4f6d42cd
Can Demircan <53577964+kaypuz@users.noreply.github.com>
2022-02-11 18:06:53 +0300

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -24,6 +24,7 @@ data_file 'VEHICLE_METADATA_FILE' 'data/**/vehicles.meta'
 data_file 'CARCOLS_FILE' 'data/**/carcols.meta'
 data_file 'VEHICLE_VARIATION_FILE' 'data/**/carvariations.meta'
 data_file 'VEHICLE_LAYOUTS_FILE' 'data/**/vehiclelayouts.meta'
+
 data_file 'AUDIO_SYNTHDATA' 'audioconfig/r34sound_amp.dat'
 data_file 'AUDIO_GAMEDATA' 'audioconfig/r34sound_game.dat'
 data_file 'AUDIO_SOUNDDATA' 'audioconfig/r34sound_sounds.dat'
